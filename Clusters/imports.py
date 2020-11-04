70fb3aa650a485ed96b97e7c943aeac3d773003b
Emily Rivera <emrivera@kean.edu>
2020-11-04 18:59:08 -0500

    Update imports.py

--- Clusters/imports.py
+++ Clusters/imports.py
@@ -1,15 +1,22 @@
+# Basics
 import numpy as np
 import time 
+
+# Figures
 import plotly.express as px
 import random as r
 import matplotlib.pyplot as plt
+
+# Data Prep 
 import pandas as pd
 import png
 import cv2
 import re
 import os
+
+# Custom files
 from load_data import *
 from prep_data import *
-from naive_kmeans import *
+# from naive_kmeans import * # excluding it because too slow
 from myplotting import *
 from cython_naive_kmeans import *
