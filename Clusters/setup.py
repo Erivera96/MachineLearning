5dc372a1445feceaebc503e01f11de9ea04e5ea8
Emily Rivera <emrivera@kean.edu>
2020-11-04 19:02:32 -0500

    Update setup.py

--- Clusters/setup.py
+++ Clusters/setup.py
@@ -1,3 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+
+# This compiles the cython code (annotate True creates the html file)
+# In the html file the more yellow means the most python it's running
+#   instead of C
 setup(ext_modules = cythonize("*.pyx", annotate=True))
